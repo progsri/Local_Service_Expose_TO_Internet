b85e842d868ddfbc95ebc7681a1ddadc2ffac165
Srikanth Lankapalli <machine.progsri@gmail.com>
2017-07-25 16:26:17 -0500

    Making Maven to compile

--- actual_service/src/main/java/com/ninjashore/actual/EndpointController.java
+++ actual_service/src/main/java/com/ninjashore/actual/EndpointController.java
@@ -3,16 +3,17 @@ package com.ninjashore.actual;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class EndpointController {
 
-    @RequestMapping("/store")
+    @RequestMapping("/presentTime")
     public String store() {
-        return "Store the public ip and port";
+
+        Date date = new Date();
+        return date.toString();
+
     }
 
-    @RequestMapping("/connect")
-    public String connect() {
-        return "connect to the listen service";
-    }
 }
